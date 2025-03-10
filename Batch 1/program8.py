a491b087ee32b7613e1fcbcdc4da765c292b1976
ycz <yczaubhreydy@gmail.com>
2025-03-10 19:19:25 +0800

    identify_append_odd_numbers

--- Batch 1/program8.py
+++ Batch 1/program8.py
@@ -1,5 +1,10 @@
 # Ask user to input 10 numbers 
 # Print the length of all odd numbers
 
+num = []
+
 for i in range (1, 11):
     num = float(input(f'Input number {i}: '))
+    if num % 2 == 0:
+        odd.append(num)
+
