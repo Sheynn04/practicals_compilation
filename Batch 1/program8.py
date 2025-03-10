4fcb71bd54b731310239dd0bcaa2c8251a5a489b
ycz <yczaubhreydy@gmail.com>
2025-03-10 19:21:42 +0800

    fixed_errors_print_len_odd

--- Batch 1/program8.py
+++ Batch 1/program8.py
@@ -1,10 +1,11 @@
 # Ask user to input 10 numbers 
 # Print the length of all odd numbers
 
-num = []
+odd = []
 
 for i in range (1, 11):
     num = float(input(f'Input number {i}: '))
-    if num % 2 == 0:
+    if num % 2 != 0:
         odd.append(num)
 
+print (len(odd))
