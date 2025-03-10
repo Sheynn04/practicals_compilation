cf68bd8c876db81b2de18f399c0478f725e3a1ad
ycz <yczaubhreydy@gmail.com>
2025-03-10 18:52:19 +0800

    remove_space_after_float

--- Batch 1/program1.py
+++ Batch 1/program1.py
@@ -1,8 +1,8 @@
 # Ask user to input 2 numbers
 # Print bigger the bigger number
 
-num1 = float (input('Input 1st number: '))
-num2 = float (input('Input 2nd number: '))
+num1 = float(input('Input 1st number: '))
+num2 = float(input('Input 2nd number: '))
 
 if num1 > num2:
     print (num1)
