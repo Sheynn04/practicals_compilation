c3dbd54e117a6c82f733d70350885bb181e3b9e6
ycz <yczaubhreydy@gmail.com>
2025-03-10 18:54:21 +0800

    remove_space_after_float

--- Batch 1/program3.py
+++ Batch 1/program3.py
@@ -1,7 +1,7 @@
 # Ask user to input 2 numbers
 # Print the sum of two numbers
 
-num1 = float (input('Input 1st number: '))
-num2 = float (input('Input 2nd number: '))
+num1 = float(input('Input 1st number: '))
+num2 = float(input('Input 2nd number: '))
 
 print(num1+num1)
