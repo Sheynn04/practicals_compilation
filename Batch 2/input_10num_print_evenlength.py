08064ec102d6fb17725a1b49941443ad93fc56e8
ycz <yczaubhreydy@gmail.com>
2025-03-10 21:37:15 +0800

    even_append_list

--- Batch 2/input_10num_print_evenlength.py
+++ Batch 2/input_10num_print_evenlength.py
@@ -1,6 +1,10 @@
 # Ask user to input 10 numbers
 # Display the length of all the even numbers they inputted
 
+even = []
+
 for i in range (10):
     num = float(input(f'Enter number {i+1}: '))
-    
+    if num % 2 == 0:
+        even.append(num)
+print (even) 
