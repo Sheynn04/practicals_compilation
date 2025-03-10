0967e9e90fc6705d8ca8e05e3e185fdaf6aa0f5f
ycz <yczaubhreydy@gmail.com>
2025-03-10 18:36:35 +0800

    print_product

--- Batch 1/program4.py
+++ Batch 1/program4.py
@@ -1,5 +1,7 @@
 # Ask user to input 2 numbers
 # Print the product of the two numbers
 
-num1 = float (input('Input 1st number: '))
-num2 = float (input('Input 2nd number: '))
+num1 = float(input('Input 1st number: '))
+num2 = float(input('Input 2nd number: '))
+
+print (num1*num2)
