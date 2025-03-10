5a43ca3058fad8099ca97a18cf978a359a2ada8f
ycz <yczaubhreydy@gmail.com>
2025-03-10 18:53:25 +0800

    remove_space_after_float

--- Batch 1/program2.py
+++ Batch 1/program2.py
@@ -1,8 +1,8 @@
 # Ask user to input 2 numbers
 # Print equal if numbers are the same
 
-num1 = float (input('Input 1st number: '))
-num2 = float (input('Input 2nd number: '))
+num1 = float(input('Input 1st number: '))
+num2 = float(input('Input 2nd number: '))
 
 if num1 == num2:
     print ("Equal")
