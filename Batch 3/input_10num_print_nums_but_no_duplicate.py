c23ba1df5cbea01ba4d18e491c16416955474882
ycz <yczaubhreydy@gmail.com>
2025-03-11 22:08:01 +0800

    raw_nums_list

--- Batch 3/input_10num_print_nums_but_no_duplicate.py
+++ Batch 3/input_10num_print_nums_but_no_duplicate.py
@@ -1,6 +1,11 @@
 # Ask the user to input 10 numbers
 # Print all the numbers including ones with duplicates but only print them once
 
+raw_nums_list = []
+
 for i in range(10):
     num_input = int(input(f'Input number {i+1}: '))
+    raw_nums_list.append(num_input)
+
+print(raw_nums_list)
 
