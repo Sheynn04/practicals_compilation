7e1162ad13ebbc765d6cacbf13bb891505741912
ycz <yczaubhreydy@gmail.com>
2025-03-11 21:29:40 +0800

    print_numbers_between_nums_inputted

---
+++ Batch 2/input_2num_print_nums_in_between.py
@@ -0,0 +1,8 @@
+# Ask the user to input 2 numbers
+# Print all the numbers in between of the 2 numbers
+
+num1 = int(input('Input first number: '))
+num2 = int(input('Input first number: '))
+
+for i in range(num1 + 1, num2):
+    print(i)
