c86cb3235af4cf51ba5d096f9d5b3021ae197ba3
ycz <yczaubhreydy@gmail.com>
2025-03-11 21:41:50 +0800

    still_outputs_duplicate

--- Batch 3/program1.py
+++ Batch 3/program1.py
@@ -1,5 +1,11 @@
 # Ask user to input 10 numbers
 # Display all numbers without duplicates
 
+non_dupe_nums = []
+
 for i in range(10):
     num_input = int(input(f'Input number {i+1}: '))
+    if num_input != non_dupe_nums:
+        non_dupe_nums.append(num_input)
+
+print(non_dupe_nums)
