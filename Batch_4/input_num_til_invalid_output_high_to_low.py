639f290beda40a62726bbf2f88b14606e4055062
ycz <yczaubhreydy@gmail.com>
2025-03-13 15:26:52 +0800

    numbers_input_loop

---
+++ Batch_4/input_num_til_invalid_output_high_to_low.py
@@ -0,0 +1,9 @@
+# Create a program that ask user to input a number, 
+# continue asking until the user input is invalid. 
+# Display the number from highest to lowest. 
+# Clue: sort() function
+
+while True:
+    numbers = int(input("Input numbers: "))
+
+    
