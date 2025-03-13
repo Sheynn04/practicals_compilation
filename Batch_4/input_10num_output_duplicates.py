830fd9af83da33cf7b273feb39352a2e6550c8c4
ycz <yczaubhreydy@gmail.com>
2025-03-13 14:51:54 +0800

    error_but_progress

--- Batch_4/input_10num_output_duplicates.py
+++ Batch_4/input_10num_output_duplicates.py
@@ -1,5 +1,17 @@
 # Create a program that ask user to input 10 numbers. 
 # Display all numbers that have duplicate.
 
+num_list = []
+dupe_list = []
 for i in range(10):
     number = int(input(f"Input number {i+1}: "))
+    num_list.append(number)
+
+    if number in num_list:
+        dupe_list.append(number)
+
+print(dupe_list)
+        
+
+
+
