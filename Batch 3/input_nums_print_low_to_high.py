44c506a3904356363cd3de72c4f2547c6da23d8d
ycz <yczaubhreydy@gmail.com>
2025-03-13 14:33:49 +0800

    append_and_sort_functions

--- Batch 3/input_nums_print_low_to_high.py
+++ Batch 3/input_nums_print_low_to_high.py
@@ -2,10 +2,16 @@
 # continue asking until the user input is invalid. 
 # Display the number from lowest to highest. Clue: sort() function
 
+num_list = []
 while True:
+    
     try:
         number = int(input("Enter number: "))
+        num_list.append(number)
+
     except ValueError:
+        num_list.sort()
+        print(num_list)
         exit()
 
     
