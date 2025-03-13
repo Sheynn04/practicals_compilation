c9d2dc1607da684f367c82298288fad8bd356164
ycz <yczaubhreydy@gmail.com>
2025-03-13 14:15:59 +0800

    number_input_loop

---
+++ Batch 3/input_num_display_lowest.py
@@ -0,0 +1,6 @@
+# Create a program that ask user to input a number, 
+# continue asking until the user input is invalid. 
+# Display the lowest number
+
+while True:
+    number = int(input("Input a number: "))
