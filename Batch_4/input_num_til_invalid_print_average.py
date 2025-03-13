d9ab4100440cf8be1185a8e37acb068b6c6b3c2e
ycz <yczaubhreydy@gmail.com>
2025-03-13 17:58:35 +0800

    fixed_code_displaying_average_when_valid

--- Batch_4/input_num_til_invalid_print_average.py
+++ Batch_4/input_num_til_invalid_print_average.py
@@ -2,10 +2,13 @@
 # continue asking until the user input is invalid. 
 # Display the average.
 num_list = []
+
 while True:
     try:
         numbers = int(input("Input number: "))
         num_list.append(numbers)
+        average = sum(num_list) / len(num_list)
     except ValueError:
-        print(sum(num_list)/len(num_list))
-        exit()
+        if num_list:
+            print(average)
+        break
