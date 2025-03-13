81ae8865337c5cbfd39dcbeab33aa57d625e5bc9
ycz <yczaubhreydy@gmail.com>
2025-03-13 14:21:40 +0800

    input_til_invalid_display_lowest

--- Batch 3/input_num_display_lowest.py
+++ Batch 3/input_num_display_lowest.py
@@ -2,8 +2,12 @@
 # continue asking until the user input is invalid. 
 # Display the lowest number
 
+num_list = []
 while True:
     try:
         number = int(input("Input a number: "))
+        num_list.append(number)
     except ValueError:
+        print(min(num_list))
         exit()
+
