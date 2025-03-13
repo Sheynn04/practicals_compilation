3925ad47dab3a32dacbfe352bfe3dd2a95bf6c27
ycz <yczaubhreydy@gmail.com>
2025-03-13 14:53:51 +0800

    fixed_display_num_with_dupes

--- Batch_4/input_10num_output_duplicates.py
+++ Batch_4/input_10num_output_duplicates.py
@@ -2,15 +2,14 @@
 # Display all numbers that have duplicate.
 
 num_list = []
-dupe_list = []
+
 for i in range(10):
     number = int(input(f"Input number {i+1}: "))
     num_list.append(number)
 
-    if number in num_list:
-        dupe_list.append(number)
-
-print(dupe_list)
+dupe_list = [num for num in num_list if num_list.count(num) > 1]
+print(set(dupe_list))
+        
         
 
 
